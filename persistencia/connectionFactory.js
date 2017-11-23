792ed75f209f07e9840f0450c5c22bfcb193e2c4
Ayla <fernandes.ayla@gmail.com>
2017-11-23 19:45:15 -0200

    docker funcionando

--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -2,8 +2,8 @@ var mysql  = require('mysql');
 
 function createDBConnection(){
   return mysql.createConnection({
-   host: '0.0.0.0',
-  //  host: 'dicre2.intranet.bb.com.br',
+   //host: '0.0.0.0',
+    host: 'dicre2.intranet.bb.com.br',
     user: 'root',
     password: 'credidf',
     database: 'teste',
